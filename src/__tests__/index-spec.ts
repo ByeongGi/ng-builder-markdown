86d878f0dc58c54f2a280c3f2253fb8d5021d6e8
ChristianKim <christian@swit.io>
2020-08-14 00:38:43 +0900

    chore: 테스트 코드 주석 제거

--- src/__tests__/index-spec.ts
+++ src/__tests__/index-spec.ts
@@ -16,20 +16,12 @@ describe('Command Runner Builder', () => {
   beforeEach(async () => {
     const registry = new schema.CoreSchemaRegistry();
     registry.addPostTransform(schema.transforms.addUndefinedDefaults);
-    // Arguments to TestingArchitectHost are workspace and current directories.
-    // Since we don't use those, both are the same in this case.
-
     architectHost = new TestingArchitectHost(projectRoot, projectRoot);
     architect = new Architect(architectHost, registry);
-
-
-    // This will either take a Node package name, or a path to the directory
-    // for the package.json file.
     await architectHost.addBuilderFromPackage(projectRoot);
     console.log('##', Array.from((architectHost as any)._builderMap.keys()));
   });
 
-  // This might not work in Windows.
   it('Markdown File을 읽어 변환하여 File의 정보를 가져온다 ', async (done) => {
     const logger = new logging.Logger('ng-markdown:markdown');
     const logs: LogEntry[] = [];
@@ -37,7 +29,6 @@ describe('Command Runner Builder', () => {
       logs.push(ev);
     });
 
-    // A "run" can contain multiple outputs, and contains progress information.
     const run = await architect.scheduleBuilder('ng-markdown:markdown', <Options>{
       input: './src/__tests__/markdown',
       output: {
@@ -50,10 +41,6 @@ describe('Command Runner Builder', () => {
       }
 
     }, {logger});
-
-    // We pass the logger for checking later.
-    // The "result" member is the next output of the runner.
-    // This is of type BuilderOutput.
     const output = await run.result;
     logs.forEach((ev) => {
       if (ev.message === NGMarkdownEvent.FILE_INFO_RESULT
